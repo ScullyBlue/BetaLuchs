80da3dc586658e2a002e46dd61bc89bc85732700
Jan <jackflash5500@gmail.com>
2017-10-27 18:40:38 +0200

    Projektdateien hinzufügen.

---
+++ BetaLuchs/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BetaLuchs.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
