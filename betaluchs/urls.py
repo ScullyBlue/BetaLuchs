2b634bf3ef3dde9a673a3ef9a62d520d06114168
Jan <jackflash5500@gmail.com>
2017-12-29 15:25:43 +0100

    Added an index

--- betaluchs/urls.py
+++ betaluchs/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('projects', views.projects, name='projects'),
+    path('project/<int:project_id>/', views.project_detail, name='project_detail'),
 ]
--- betaluchs/views.py
+++ betaluchs/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from betaluchs.models import Project
-
+from django.shortcuts import render, get_object_or_404
 
 def projects(request):
     return HttpResponse(Project.objects.all())
+
+
+def index(request):
+    project_list = Project.objects.all()[:5]
+    context = {
+        'project_list': project_list,
+    }
+    return render(request, 'betaluchs/index.html', context)
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    return render(request, 'betaluchs/project_detail.html', {'project': project})
